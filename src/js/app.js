d58855a9fc7143f1c99776e871380c67023c3e84
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-04-16 21:48:34 +0900

    loading voice list

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,10 @@
 import 'babel-polyfill';
 
 document.addEventListener('DOMContentLoaded', () => {
-  function synthesis(token, text) {
+  function synthesis(token, text, voiceConfig) {
     const request = {};
     request.input = { text: text };
-    // request.voice = { languageCode: 'en-gb', name: 'en-GB-Standard-A', ssmlGender: 'FEMALE' };
-    request.voice = { languageCode: 'ja-jp', ssmlGender: 'FEMALE' };
+    request.voice = voiceConfig;
     request.audioConfig = { audioEncoding: 'MP3' };
 
     return fetch("https://texttospeech.googleapis.com/v1beta1/text:synthesize", {
@@ -28,16 +27,42 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const context = new window.AudioContext();
+  const languageCode = 'ja-JP';
 
+  let token;
   fetch('/credential.json')
     .then((response) => { return response.json(); })
     .then((credentail) => {
-      const token = credentail.token;
+      token = credentail.token;
+      return fetch("https://texttospeech.googleapis.com/v1beta1/voices", {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': `Bearer ${token}`
+        })
+      })
+    })
+    .then((response) => { return response.json(); })
+    .then((json) => {
+      const select = document.querySelector("select[name=voice]");
+
+      json.voices.forEach((val,index,ar) => {
+        if (! val.languageCodes.includes(languageCode)) {
+          return;
+        }
+
+        const option = document.createElement("option");
+        option.appendChild(document.createTextNode(val.name));
+        option.defaultValue = val.name;
+        option.dataset.ssmlGender = val.ssmlGender;
+        select.appendChild(option);
+      });
 
       const btn = document.querySelector("button[name=synthesis]");
       const text = document.querySelector("textarea[name=text]");
       btn.onclick = (ev) => {
-        synthesis(token, text.value)
+        const option = select.querySelectorAll('option:checked')[0];
+
+        synthesis(token, text.value, { languageCode: languageCode, name: option.value, ssmlGender: option.dataset.ssmlGender })
           .then((response) => { return response.json(); })
           .then((response) => {
             const base64Audio = response.audioContent;
